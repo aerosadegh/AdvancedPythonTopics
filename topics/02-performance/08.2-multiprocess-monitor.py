34bf01b6cc0a94db49604d80aa29915e27cb732d
Sadegh Yazdani <silverstar10@gmail.com>
2023-07-24 11:34:55 +0330

    perf: update `multiprocessing.monitor_func`

--- topics/02-performance/08.2-multiprocess-monitor.py
+++ topics/02-performance/08.2-multiprocess-monitor.py
@@ -1,6 +1,6 @@
-import sys
 import time
-import multiprocessing
+from multiprocessing import Process, Event
+from multiprocessing import synchronize
 
 DELAY = 0.1
 
@@ -11,31 +11,31 @@ def get_duration(start, end) -> str:
     return f"{int(hours):0>2}:{int(minutes):0>2}:{seconds:05.2f}"
 
 
-def monitor_func(t0, welcome_message):
-    write, flush = sys.stdout.write, sys.stdout.flush
+def monitor_func(t0, done: synchronize.Event, before="", after="") -> None:
     t1 = time.time()
-    write(f"{welcome_message}\n")
     while True:
-        msg = get_duration(t0, t1)
-        write(msg)
-        flush()
-        write("\x08" * len(msg))
-        time.sleep(DELAY)
+        msg = before + get_duration(t0, t1) + after
+        status = f"\r {msg}"
+        print(status, end="", flush=True)
+        if done.wait(DELAY):
+            break
         t1 = time.time()
+    blanks = " " * len(status)
+    print(f"\r{blanks}\r", end="")
 
 
 def long_computation():
     # emulate a long computation
     time.sleep(3)
+    return 42
 
 
 if __name__ == "__main__":
-    spinner = multiprocessing.Process(
-        None, monitor_func, args=(time.time(), "Start Monitoring\nPlease wait ... ")
-    )
+    done = Event()
+    spinner = Process(None, monitor_func, args=(time.time(), done, "Please wait ... "))
     spinner.start()
-    try:
-        long_computation()
-        print("\nComputation done")
-    finally:
-        spinner.terminate()
+    res = long_computation()
+    done.set()
+    print()  # keep progress time
+    spinner.join()
+    print(f"Answer: {res}")
